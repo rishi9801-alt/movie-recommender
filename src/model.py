979dae38abfb02bc04afed3f8fb8bea2c4a5a902
Rishav raj <rishi9520raj@gmail.com>
2026-04-21 00:08:22 +0530

    update model and cleanup project structure

--- src/model.py
+++ src/model.py
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
+
 import os
-=======
->>>>>>> 8e7a4f7f8d27fd09975f26a21c6dece6d436772d
+
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from preprocess import preprocess
-
-<<<<<<< HEAD
 
 def create_model():
     # ✅ FIX: ensure folder exists
@@ -32,7 +29,6 @@ def create_model():
 
     print("Model created and saved successfully!")
 
-=======
 def create_model():
     new_df = preprocess()
 
@@ -46,7 +42,6 @@ def create_model():
     pickle.dump(similarity, open('artifacts/similarity.pkl', 'wb'))
 
     print("Model created and saved!")
->>>>>>> 8e7a4f7f8d27fd09975f26a21c6dece6d436772d
 
 if __name__ == "__main__":
     create_model()
